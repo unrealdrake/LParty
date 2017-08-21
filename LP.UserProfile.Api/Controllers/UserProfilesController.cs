dfe96db52383b7c0829c8049847e1d746735be30
Igor <unrealdrake@gmail.com>
2017-08-22 01:01:04 +0300

    UserProfile gateway

---
+++ LP.UserProfile.Api/Controllers/UserProfilesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LP.UserProfile.Gateway.Models;
+
+namespace LP.UserProfile.Api.Controllers
+{
+    [Route("userProfile/api/[controller]")]
+    public class UserProfilesController : Controller
+    {
+        //// GET api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        [HttpPost]
+        public void Post([FromBody]RegisterNewProfileModel model)
+        {
+        }
+    }
+}
