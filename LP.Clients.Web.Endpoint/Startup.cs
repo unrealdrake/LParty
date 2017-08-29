e528465a6d7cc26a71995dd9f1cf344b5ee3a93c
Игорь <unrealdrake@gmail.com>
2017-08-29 22:53:47 +0300

    web client core implementation

---
+++ LP.Clients.Web.Endpoint/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LP.Clients.Web.Endpoint
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                {
+                    HotModuleReplacement = false
+                });
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
---
+++ LP.Clients.Web.Endpoint/webpack.config.js
@@ -0,0 +1,26 @@
+﻿var path = require('path');
+var merge = require('webpack-merge');
+
+var isDevelopment = process.env.ASPNETCORE_ENVIRONMENT === 'Development';
+
+module.exports = merge({
+  resolve: {
+    extensions: ['.js', '.ts'],
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  module: {
+    loaders: [
+      {test: /\.ts(x?)$/, exclude: /node_modules/, loader: 'ts-loader?silent'}
+    ]
+  },
+  entry: {
+    main: './Scripts/App.ts'
+  },
+  output: {
+    path: path.join(__dirname, 'wwwroot', 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist/'
+  }
+});
