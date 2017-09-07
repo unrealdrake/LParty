ca3b08d9f7b6eab13a2648abb51f71fd251da695
Igor <unrealdrake@gmail.com>
2017-09-07 23:43:59 +0300

    css restyling

--- LP.Clients.Web.Endpoint/Startup.cs
+++ LP.Clients.Web.Endpoint/Startup.cs
@@ -1,8 +1,11 @@
-﻿using LP.Clients.Web.Endpoint.Infrastructure;
+﻿using System.IO;
+using LP.Clients.Web.Endpoint.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Shared.CompositionRoot;
 
 namespace LP.Clients.Web.Endpoint
@@ -22,6 +25,12 @@ namespace LP.Clients.Web.Endpoint
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseStaticFiles(); // For the wwwroot folder
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"Features")),
+                RequestPath = new PathString("/Features")
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
