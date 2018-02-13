bda67dee6420ef7db2ea56ae113d5dd493008ccf
Igor <unrealdrake@gmail.com>
2018-02-13 22:50:08 +0300

    WEB angular part initial core implementation

---
+++ Clients/Web/Clients.Web.Endpoint/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clients.Web.Endpoint.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
