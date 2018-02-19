483bd3af75407bbba498b4f2e54cf16883ff78b9
Teryohin, Igor <i.teryohin@itransition.com>
2018-02-19 19:55:01 +0300

    WEB App cleaning

--- Clients/Web/Clients.Web.Endpoint/ClientApp/app/app.shared.module.ts
+++ Clients/Web/Clients.Web.Endpoint/ClientApp/app/app.shared.module.ts
@@ -5,29 +5,21 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
+        LoginComponent
     ],
     imports: [
         CommonModule,
         HttpModule,
         FormsModule,
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
+            { path: 'login', component: LoginComponent },
+            { path: '**', redirectTo: 'login' }
         ])
     ]
 })
---
+++ Clients/Web/Clients.Web.Endpoint/ClientApp/app/components/login/login.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+    email = "";
+    password = "";
+    login(): void {
+
+    }
+}
